b5e91ceb64b0bbd96708e12897f04d3c6eb559e5
Joris Mansion <juhh@juhh-core.lan>
2024-06-17 18:04:11 +0200

    remove navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,39 +1,16 @@
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home.tsx";
 import "./index.css"
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-import Navbar from "./components/Navbar.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Effect from "./pages/Effect.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "effect",
-        element: <Effect/>,
-      },
-    ],
+    element: <Effect />,
   },
 ]);
 
-function Root() {
-  return (
-    <>
-      <header>
-        <Navbar />
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </>
-  );
-}
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
